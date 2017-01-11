3470b2aedb025a94596ea6d7fad1c737481c3aa3
misawakenichi <misawa0724kenichi@gmail.com>
2017-01-11 18:34:51 +0900

    svg追加

--- scripts/roulette.js
+++ scripts/roulette.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
 import game from "./game";
 
+
 const page = document.querySelector("#page-roulette");
-const emotions = ["fear", "happiness", "neutral", "sadness"];
-const face_name = ["怖がってる顔", "幸せな顔", "真顔", '悲しい顔'];
+const emotions = ["anger", "contempt", "disgust", "fear", "hapiness", "neutral", "sadness", "surprise"];
 var rand_num = 0;
 
 export default function roulette() {
@@ -12,7 +12,6 @@ export default function roulette() {
     var shuffle_emotion = () => {
         rand_num = Math.floor(Math.random() * emotions.length);
         $('.emotion_icon img').attr("src",'img/roulette_icon/'+emotions[rand_num]+'.svg');
-        $('#page-roulette p').text(face_name[rand_num]);
         i++;
         if(i < 30) {
             setTimeout(shuffle_emotion, 100);
